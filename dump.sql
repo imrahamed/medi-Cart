5d4250d9a1d1099b5e93779d52ed89d56569990b
root <root@cdcedfdddaafcdcffeeaaceefcfefccebbec-0.cdcedfdddaafcdcffeeaaceefcfefccebbec.che.svc.cluster.local>
2020-03-19 11:24:10 +0000

    comment updated

--- dump.sql
+++ dump.sql
@@ -61,7 +61,7 @@ CREATE TABLE `category` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `category`
+-- Dumping datas for table `category`
 --
 
 LOCK TABLES `category` WRITE;
